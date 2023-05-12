7f893b9a461403aeff570d000abf59349ec64c6c
mons110 <vadim01152005@gmail.com>
2023-05-12 12:19:37 +0300

    task11

---
+++ WebServer/Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace WebServer.Auth
+{
+	public class CustomAuthStateProvider: AuthenticationStateProvider
+    {
+		private readonly ProtectedSessionStorage _sessionStorage;
+		private readonly ProtectedLocalStorage _localStorage
+    }
+}
